967ac310ee3adf24685cc105914076ff49c5ff5c
Don Schenck <don.schenck@gmail.com>
2016-08-25 11:47:58 -0400

    Restored HelloMvcApi to match book.

--- HelloMvcApi/Controllers/ProductsController.cs
+++ HelloMvcApi/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMvcApi
@@ -8,18 +9,9 @@ namespace HelloMvcApi
     {
         private static List<Product> _products = 
             new List<Product>(new[] {
-            new Product() { Id = 1, Name = "Cooler" },
+            new Product() { Id = 1, Name = "Computer" },
             new Product() { Id = 2, Name = "Radio" },
-            new Product() { Id = 3, Name = "Bicycle" },
-            new Product() { Id = 4, Name = "Kayak" },
-            new Product() { Id = 5, Name = "Surfboard" },
-            new Product() { Id = 6, Name = "Blanket" },
-            new Product() { Id = 7, Name = "Suntan lotion" },
-            new Product() { Id = 8, Name = "Beach chair" },
-            new Product() { Id = 9, Name = "Umbrella" },
-            new Product() { Id = 10, Name = "Sunglasses" },
-            new Product() { Id = 11, Name = "Camera" },
-            new Product() { Id = 12, Name = "Binoculars" },
+            new Product() { Id = 3, Name = "Apple" },
         });
 
         public IEnumerable<Product> Get()
